e21fd86cbd6b326e66185b3e543bf7bb653d5f40
missyraffy <129610050+missyraffy@users.noreply.github.com>
2023-04-01 11:24:51 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e a a a 5 5 a a a e e . . . 
+    . . e a f a 5 5 a f a e . . . . 
+    . . f a a a a a a a a f . f f . 
+    . . . 4 5 5 f f 5 5 6 f f 5 f . 
+    . . . f 6 6 6 6 6 6 4 f 5 5 f . 
+    . . . f 5 5 5 5 5 5 5 4 5 f . . 
+    . . . . f 5 4 5 f 5 f f f . . . 
+    . . . . . f f f f f f f . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+scene.setBackgroundColor(7)
+info.setLife(3)
+scene.cameraFollowSprite(mySprite)
+tiles.setCurrentTilemap(tilemap`level2`)
